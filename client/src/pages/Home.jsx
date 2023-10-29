719eef34a84f5bf5ac7bccc5de83569f3c62ce72
kpsingh-20 <pkrishnapal08@gmail.com>
2023-10-29 17:47:23 +0530

    fixed bug home page slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -79,7 +79,7 @@ useEffect(() =>{
           <br />
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Pro Real Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
